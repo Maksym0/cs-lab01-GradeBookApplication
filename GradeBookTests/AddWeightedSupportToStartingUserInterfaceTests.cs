2857391aff1e0169ec963eba0eeed04e428a6346
Maksym0 <93425561+Maksym0@users.noreply.github.com>
2022-04-03 14:15:54 +0200

    Update AddWeightedSupportToStartingUserInterfaceTests.cs

--- GradeBookTests/AddWeightedSupportToStartingUserInterfaceTests.cs
+++ GradeBookTests/AddWeightedSupportToStartingUserInterfaceTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using GradeBook.UserInterfaces;
 using Xunit;
@@ -7,13 +7,9 @@ namespace GradeBookTests
 {
     public class AddWeightedSupportToStartingUserInterfaceTests
     {
-        /// <summary>
-        ///     Tests Help Command update to ensure all changes were made correctly.
-        /// </summary>
         [Fact(DisplayName = "Update HelpCommand Method @update-helpcommand-method")]
         public void UpdateHelpCommandTest()
         {
-            //Setup Test
             var output = string.Empty;
 
             try
@@ -27,7 +23,6 @@ namespace GradeBookTests
                         StartingUserInterface.HelpCommand();
                         output = consolestream.ToString().ToLower();
 
-                        // Test if help command message is correct
                         Assert.True(output.Contains("create 'name' 'type' 'weighted' - creates a new gradebook where 'name' is the name of the gradebook, 'type' is what type of grading it should use, and 'weighted' is whether or not grades should be weighted (true or false)."), "`GradeBook.UserInterfaces.StartingUserInterface.HelpCommand` didn't write \"Create 'Name' 'Type' 'Weighted' - Creates a new gradebook where 'Name' is the name of the gradebook, 'Type' is what type of grading it should use, and 'Weighted' is whether or not grades should be weighted (true or false).\"");
                     }
                 }
