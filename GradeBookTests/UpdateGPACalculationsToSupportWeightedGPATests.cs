398859b2e62010421d77a6256a00bd0defa32b39
Maksym0 <93425561+Maksym0@users.noreply.github.com>
2022-04-03 12:15:14 +0200

    Update UpdateGPACalculationsToSupportWeightedGPATests.cs

--- GradeBookTests/UpdateGPACalculationsToSupportWeightedGPATests.cs
+++ GradeBookTests/UpdateGPACalculationsToSupportWeightedGPATests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Reflection;
 using GradeBook.Enums;
@@ -9,22 +9,16 @@ namespace GradeBookTests
 {
     public class UpdateGPACalculationsToSupportWeightedGPATests
     {
-        /// <summary>
-        ///     All Tests related to the "Update BaseGradeBooks GetGPA Method" Task.
-        ///</summary>
         [Fact(DisplayName = "Update BaseGradeBooks GetGPA Method Test @update-basegradebook-s-getgpa-method")]
         public void GetWeightedGPATest()
         {
             var standardGradeBook = TestHelpers.GetUserType("GradeBook.GradeBooks.StandardGradeBook");
             Assert.True(standardGradeBook != null, "`GradeBook.GradeBooks.StandardGradeBook` doesn't exist.");
 
-            // Test if `StandardGradeBook` is `public`.
             Assert.True(standardGradeBook.IsPublic, "`GradeBook.GradeBooks.StandardGradeBook` isn't public");
 
-            // Test if `StandardGradeBook` is inheriting `BaseGradeBook`.
             Assert.True(standardGradeBook.BaseType == typeof(BaseGradeBook), "`GradeBook.GradeBooks.StandardGradeBook` doesn't inherit `BaseGradeBook`");
 
-            // Test if `StandardGradeBook`'s constructor has the proper signature (consider both this task and later signature)
             var ctor = standardGradeBook.GetConstructors().FirstOrDefault();
             Assert.True(ctor != null, "No constructor found for GradeBook.GradeBooks.StandardGradeBook.");
 
@@ -35,12 +29,10 @@ namespace GradeBookTests
             gradeBook = Activator.CreateInstance(standardGradeBook, "WeightedTest", true);
             MethodInfo method = standardGradeBook.GetMethod("GetGPA");
 
-            // Test weighting works correctly for Weighted gradebooks
+   
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.Standard }) == 4, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method weighted a student's grade even when they weren't an Honors or Duel Enrolled student.");
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.Honors }) == 5, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method did not weight a student's when they were an Honors student.");
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.DualEnrolled }) == 5, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method did not weight a student's when they were a Dual Enrolled student.");
-
-            // Test weighting works correctly for unweighted gradebooks
             gradeBook.GetType().GetProperty("IsWeighted").SetValue(gradeBook, false);
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.Standard }) == 4, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method weighted a student's grade when the gradebook was not weighted.");
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.Honors }) == 4, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method weighted a student's grade when the gradebook was not weighted.");
@@ -48,3 +40,4 @@ namespace GradeBookTests
         }
     }
 }
+
