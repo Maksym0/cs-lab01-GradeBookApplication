c827e41ae8370a5432bd3967f77d1d2fb90533ac
Maksym0 <93425561+Maksym0@users.noreply.github.com>
2022-04-03 14:06:59 +0200

    Update OverrideRankedGradeBooksGetLetterGradeTests.cs

--- GradeBookTests/OverrideRankedGradeBooksGetLetterGradeTests.cs
+++ GradeBookTests/OverrideRankedGradeBooksGetLetterGradeTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,13 +12,9 @@ namespace GradeBookTests
 {
     public class OverrideRankedGradeBooksGetLetterGradeTests
     {
-        /// <summary>
-        ///   All tests related to the "Create GetLetterGrade Override" task.
-        /// </summary>
         [Fact(DisplayName = "Create GetLetterGrade Override @create-getlettergrade-override")]
         public void CreateGetLetterGradeOverrideTests()
         {
-            // Setup Test
             var rankedGradeBook = TestHelpers.GetUserType("GradeBook.GradeBooks.RankedGradeBook");
             Assert.True(rankedGradeBook != null, "`RankedGradeBook` wasn't found in the `GradeBooks.GradeBook` namespace.");
 
@@ -31,12 +27,9 @@ namespace GradeBookTests
                 gradeBook = Activator.CreateInstance(rankedGradeBook, "Test GradeBook");
 
             MethodInfo method = rankedGradeBook.GetMethod("GetLetterGrade");
-
-            //Test if exception is thrown when there are less than 5 students.
             var exception = Record.Exception(() => method.Invoke(gradeBook, new object[] { 100 }));
             Assert.True(exception != null, "`GradeBook.GradeBooks.RankedGradeBook.GetLetterGrade` didn't throw an exception when less than 5 students have grades.");
 
-            //Setup successful conditions
             var students = new List<Student>
             {
                 new Student("jamie",StudentType.Standard,EnrollmentType.Campus)
@@ -62,12 +55,8 @@ namespace GradeBookTests
             };
 
             gradeBook.GetType().GetProperty("Students").SetValue(gradeBook, students);
-
-            //Test if A is given when input grade is in the top 20%.
             Assert.True((char)method.Invoke(gradeBook, new object[] { 0 }) == 'F', "`GradeBook.GradeBooks.RankedGradeBook.GetLetterGrade` didn't give an F when a student failed to earn a higher grade.");
 
-            // Read file for to confirm GetLetterGrade has been overriden
-            // Get appropriate path to file for the current operating system
             var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "GradeBook" + Path.DirectorySeparatorChar + "GradeBooks" + Path.DirectorySeparatorChar + "RankedGradeBook.cs";
             var input = File.ReadAllText(filePath);
 
@@ -88,13 +77,9 @@ namespace GradeBookTests
             }
         }
 
-        /// <summary>
-        ///   All tests related to the "Top 20 Percent Get An A" task.
-        /// </summary>
         [Fact(DisplayName = "Top 20 Percent Get An A @override-getlettergrade-a")]
         public void TopTwentyPercentGetAnATests()
         {
-            // Setup Test
             var rankedGradeBook = TestHelpers.GetUserType("GradeBook.GradeBooks.RankedGradeBook");
             Assert.True(rankedGradeBook != null, "`RankedGradeBook` wasn't found in the `GradeBooks.GradeBook` namespace.");
 
@@ -108,7 +93,6 @@ namespace GradeBookTests
 
             MethodInfo method = rankedGradeBook.GetMethod("GetLetterGrade");
 
-            //Setup successful conditions
             var students = new List<Student>
             {
                 new Student("jamie",StudentType.Standard,EnrollmentType.Campus)
@@ -135,11 +119,9 @@ namespace GradeBookTests
 
             gradeBook.GetType().GetProperty("Students").SetValue(gradeBook, students);
 
-            //Test if A is given when input grade is in the top 20%.
             Assert.True((char)method.Invoke(gradeBook, new object[] { 100 }) == 'A', "`GradeBook.GradeBooks.RankedGradeBook.GetLetterGrade` didn't give an A to students in the top 20% of the class.");
 
-            // Read file for to confirm GetLetterGrade has been overriden
-            // Get appropriate path to file for the current operating system
+
             var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "GradeBook" + Path.DirectorySeparatorChar + "GradeBooks" + Path.DirectorySeparatorChar + "RankedGradeBook.cs";
             var input = File.ReadAllText(filePath);
 
@@ -160,13 +142,9 @@ namespace GradeBookTests
             }
         }
 
-        /// <summary>
-        ///   All tests related to the "Second 20 Percent Get A B" task.
-        /// </summary>
         [Fact(DisplayName = "Second 20 Percent Get An B @override-getlettergrade-b")]
         public void SecondTwentyPercentGetABTests()
         {
-            // Setup Test
             var rankedGradeBook = TestHelpers.GetUserType("GradeBook.GradeBooks.RankedGradeBook");
             Assert.True(rankedGradeBook != null, "`RankedGradeBook` wasn't found in the `GradeBooks.GradeBook` namespace.");
 
@@ -179,8 +157,6 @@ namespace GradeBookTests
                 gradeBook = Activator.CreateInstance(rankedGradeBook, "Test GradeBook");
 
             MethodInfo method = rankedGradeBook.GetMethod("GetLetterGrade");
-
-            //Setup successful conditions
             var students = new List<Student>
             {
                 new Student("jamie",StudentType.Standard,EnrollmentType.Campus)
@@ -206,18 +182,12 @@ namespace GradeBookTests
             };
 
             gradeBook.GetType().GetProperty("Students").SetValue(gradeBook, students);
-
-            //Test if B is given when input grade is between the top 20 and 40%.
             Assert.True((char)method.Invoke(gradeBook, new object[] { 75 }) == 'B', "`GradeBook.GradeBooks.RankedGradeBook.GetLetterGrade` didn't give an B to students between the top 20 and 40% of the class.");
         }
 
-        /// <summary>
-        ///   All tests related to the "Third 20 Percent Get A C" task.
-        /// </summary>
         [Fact(DisplayName = "Third 20 Percent Get An C @override-getlettergrade-c")]
         public void ThirdTwentyPercentGetACTests()
         {
-            // Setup Test
             var rankedGradeBook = TestHelpers.GetUserType("GradeBook.GradeBooks.RankedGradeBook");
             Assert.True(rankedGradeBook != null, "`RankedGradeBook` wasn't found in the `GradeBooks.GradeBook` namespace.");
 
@@ -231,7 +201,6 @@ namespace GradeBookTests
 
             MethodInfo method = rankedGradeBook.GetMethod("GetLetterGrade");
 
-            //Setup successful conditions
             var students = new List<Student>
             {
                 new Student("jamie",StudentType.Standard,EnrollmentType.Campus)
@@ -257,18 +226,12 @@ namespace GradeBookTests
             };
 
             gradeBook.GetType().GetProperty("Students").SetValue(gradeBook, students);
-
-            //Test if C is given when input grade is between the top 40 and 60%.
             Assert.True((char)method.Invoke(gradeBook, new object[] { 50 }) == 'C', "`GradeBook.GradeBooks.RankedGradeBook.GetLetterGrade` didn't give an C to students between the top 40 and 60% of the class.");
         }
 
-        /// <summary>
-        ///   All tests related to the "Fourth 20 Percent Get A D" task.
-        /// </summary>
         [Fact(DisplayName = "Fourth 20 Percent Get An D @override-getlettergrade-d")]
         public void FourthTwentyPercentGetADTests()
         {
-            // Setup Test
             var rankedGradeBook = TestHelpers.GetUserType("GradeBook.GradeBooks.RankedGradeBook");
             Assert.True(rankedGradeBook != null, "`RankedGradeBook` wasn't found in the `GradeBooks.GradeBook` namespace.");
 
@@ -282,7 +245,6 @@ namespace GradeBookTests
 
             MethodInfo method = rankedGradeBook.GetMethod("GetLetterGrade");
 
-            //Setup successful conditions
             var students = new List<Student>
             {
                 new Student("jamie",StudentType.Standard,EnrollmentType.Campus)
@@ -309,7 +271,6 @@ namespace GradeBookTests
 
             gradeBook.GetType().GetProperty("Students").SetValue(gradeBook, students);
 
-            //Test if D is given when input grade is between the top 60 and 80%.
             Assert.True((char)method.Invoke(gradeBook, new object[] { 25 }) == 'D', "`GradeBook.GradeBooks.RankedGradeBook.GetLetterGrade` didn't give an D to students between the top 60 and 80% of the class.");
         }
     }
