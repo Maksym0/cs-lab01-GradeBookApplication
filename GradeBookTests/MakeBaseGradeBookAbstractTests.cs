1f927c74d08e19919018a461723da65422d62a5e
Maksym0 <93425561+Maksym0@users.noreply.github.com>
2022-04-03 14:09:33 +0200

    Update MakeBaseGradeBookAbstractTests.cs

--- GradeBookTests/MakeBaseGradeBookAbstractTests.cs
+++ GradeBookTests/MakeBaseGradeBookAbstractTests.cs
@@ -1,17 +1,14 @@
-﻿using GradeBook.GradeBooks;
+using GradeBook.GradeBooks;
 using Xunit;
 
 namespace GradeBookTests
 {
     public class MakeBaseGradeBookAbstractTests
     {
-        /// <summary>
-        ///     All tests related to the "Make BaseGradeBook Abstract" Task.
-        /// </summary>
+
         [Fact(DisplayName = "Make BaseGradeBook Abstract @make-basegradebook-abstract")]
         public void MakeBaseGradeBookAbstract()
         {
-            // Test if `BaseGradeBook` is abstract.
             Assert.True(typeof(BaseGradeBook).IsAbstract == true, "`GradeBook.GradeBooks.BaseGradeBook` is not abstract.");
         }
     }
