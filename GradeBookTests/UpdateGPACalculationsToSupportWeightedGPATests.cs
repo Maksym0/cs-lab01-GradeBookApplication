c955b7b2efebfe63fdd72b700e380d3d1e7bd5a1
Maksym0 <93425561+Maksym0@users.noreply.github.com>
2022-04-03 15:02:07 +0200

    Update UpdateGPACalculationsToSupportWeightedGPATests.cs

--- GradeBookTests/UpdateGPACalculationsToSupportWeightedGPATests.cs
+++ GradeBookTests/UpdateGPACalculationsToSupportWeightedGPATests.cs
@@ -9,27 +9,21 @@ namespace GradeBookTests
 {
     public class UpdateGPACalculationsToSupportWeightedGPATests
     {
+
         [Fact(DisplayName = "Update BaseGradeBooks GetGPA Method Test @update-basegradebook-s-getgpa-method")]
         public void GetWeightedGPATest()
         {
             var standardGradeBook = TestHelpers.GetUserType("GradeBook.GradeBooks.StandardGradeBook");
             Assert.True(standardGradeBook != null, "`GradeBook.GradeBooks.StandardGradeBook` doesn't exist.");
-
             Assert.True(standardGradeBook.IsPublic, "`GradeBook.GradeBooks.StandardGradeBook` isn't public");
-
             Assert.True(standardGradeBook.BaseType == typeof(BaseGradeBook), "`GradeBook.GradeBooks.StandardGradeBook` doesn't inherit `BaseGradeBook`");
-
             var ctor = standardGradeBook.GetConstructors().FirstOrDefault();
             Assert.True(ctor != null, "No constructor found for GradeBook.GradeBooks.StandardGradeBook.");
-
             var parameters = ctor.GetParameters();
             object gradeBook = null;
             Assert.True(parameters.Count() == 2 && parameters[0].ParameterType == typeof(string) && parameters[1].ParameterType == typeof(bool), "`GradeBook.GradeBooks.BaseGradeBook`'s constructor doesn't have the correct parameters. It should be a `string` and a `bool`.");
-
             gradeBook = Activator.CreateInstance(standardGradeBook, "WeightedTest", true);
             MethodInfo method = standardGradeBook.GetMethod("GetGPA");
-
-   
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.Standard }) == 4, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method weighted a student's grade even when they weren't an Honors or Duel Enrolled student.");
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.Honors }) == 5, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method did not weight a student's when they were an Honors student.");
             Assert.True((double)method.Invoke(gradeBook, new object[] { 'A', StudentType.DualEnrolled }) == 5, "`GradeBook.GradeBooks.BaseGradeBook`'s `GetGPA` method did not weight a student's when they were a Dual Enrolled student.");
@@ -40,4 +34,3 @@ namespace GradeBookTests
         }
     }
 }
-
